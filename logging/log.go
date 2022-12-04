0dc60a59a3ab5245024ce8e1aff889a6ba95d9e6
arshabbir <arshabbir@gmail.com>
2022-12-04 12:25:05 +0530

    fixing the package issue

---
+++ logging/log.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"log"
+	"utilsmod/config"
+	"utilsmod/model"
+)
+
+type logger struct {
+	conf config.Config
+}
+
+type Logger interface {
+	Log(model.LogRequest)
+}
+
+func NewLogger(conf config.Config) Logger {
+	return &logger{conf: conf}
+}
+
+func (l *logger) Log(m model.LogRequest) {
+	// implement logging logic based on conf.Loglevel
+	log.Printf("\n%s : %s :  %s", m.Timestamp, m.ServiceName, m.Message)
+
+}
